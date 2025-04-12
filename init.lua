ee23d107fbfca3570c8b0b8185bf59af3f581688
javierdebug <jvargas071@gmail.com>
2025-04-12 01:33:18 -0500

    Add custom formatters (prettier and prettierd) for js, ts & html

--- init.lua
+++ init.lua
@@ -798,6 +798,11 @@ require('lazy').setup({
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        html = { 'prettierd', 'prettier' },
+        javascript = { 'prettierd', 'prettier' },
+        typescript = { 'prettierd', 'prettier' },
+        typescriptreact = { 'prettierd', 'prettier' },
+        javascriptreact = { 'prettierd', 'prettier' },
       },
     },
   },
