8c4d811fcfb5adc531ffc3681af36e195f9aa4f7
javierdebug <jvargas071@gmail.com>
2025-04-12 01:25:39 -0500

    Add custom set and remap files

---
+++ lua/javierd/set.lua
@@ -0,0 +1,6 @@
+vim.opt.relativenumber = true
+
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
